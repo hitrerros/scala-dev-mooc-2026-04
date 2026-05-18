9bc29962be383f8b588eeda96a6832d453e3cd08
avoronets84 <me@avoronets.ru>
2026-05-18 21:56:05 +0300

    webinar 4

--- src/main/scala/ru/otus/module1/App.scala
+++ src/main/scala/ru/otus/module1/App.scala
@@ -1,10 +1,9 @@
 package ru.otus.module1
 
+
 object App {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
-
-    val f: String => Unit = hof.logRunningTime(hof.dumb)
-    f("Ooops")
+    println("Hello world")
+    pattern_matching
   }
 }
