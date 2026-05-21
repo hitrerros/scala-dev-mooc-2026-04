91f1df84cbf15b84edb61f51446be0a90ae3b852
Anton <antkhit83@gmail.com>
2026-05-21 16:20:06 +0300

    Решение домашнего задания 02

---
+++ src/main/scala/ru/otus/module1/probabilities.scala
@@ -0,0 +1,26 @@
+package ru.otus.module1
+
+val rnd = new scala.util.Random()
+val BLACK = 0
+val WHITE = 1
+val numberOfElements = 10000
+
+case class BucketList(ob : List[Int]) {
+  def extractTwo : Boolean  = {
+      val r = rnd.shuffle(ob)
+      val selected = r.take(2)
+      selected.contains(WHITE)
+  }
+}
+
+object BucketList {
+   def apply() : BucketList = BucketList(List(WHITE, WHITE, WHITE, BLACK, BLACK, BLACK))
+}
+
+object probabilities extends App {
+  val buckets =  (1 to numberOfElements).map(_=>BucketList())
+  val results = buckets.map(_.extractTwo)
+  val whites: Seq[Boolean]  = results.filter(_==true)
+
+  println( s"probability =  ${(whites.size.toDouble / numberOfElements)}" )
+}
