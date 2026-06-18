351520e862b1bd4c9179e439c5805dbbd8c1f58f
Anton <antkhit83@gmail.com>
2026-06-18 14:09:06 +0300

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	build.sbt

--- src/main/scala/ru/otus/module2/10-typeClasses.scala
+++ src/main/scala/ru/otus/module2/10-typeClasses.scala
@@ -1,43 +1,57 @@
 package ru.otus.module2
 
-import ru.otus.module2.JsValue._
 import ru.otus.module2.type_classes.Eq.given_Eq_String
+import ru.otus.module2.type_classes.JsValue.{JsNull, JsNumber, JsString}
+import ru.otus.module2.type_classes.JsonWriter.{given}
 
-enum JsValue:
-  case JsObject(get: Map[String, JsValue])
-  case JsString(get: String)
-  case JsNumber(get: Double)
-  case JsNull
-// 1
 
 object type_classes extends App {
 
-   trait JsonWriter[T]:
-       def toJson(v: T): JsValue
+  sealed trait JsValue
 
-   object JsonWriter:
-    def apply[T](using ev: JsonWriter[T]) = ev
-    private val from = [T] => (f: T => JsValue) => new JsonWriter[T]:
-     override def toJson(v: T): JsValue = f(v)
+  object JsValue {
+    final case class JsObject(get: Map[String, JsValue]) extends JsValue
 
-    given JsonWriter[String] = from[String](JsString)
-    given JsonWriter[Int] = from[Int](JsNumber)
+    final case class JsString(get: String) extends JsValue
 
-    given optJson [T](using jw: JsonWriter[T]): JsonWriter[Option[T]] = from[Option[T]] :
-      case Some(value) => jw.toJson(value)
-      case None => JsNull
+    final case class JsNumber(get: Double) extends JsValue
 
-   def toJson[T: JsonWriter](v: T): JsValue =
-    summon[JsonWriter[T]].toJson(v)
+    case object JsNull extends JsValue
+  }
   
-   println(toJson("vffv"))
-   println(toJson(10))
+  // 1
+  
+  trait JsonWriter[-T] {
+    def toJson(v: T): JsValue
+  }
+  
+  object JsonWriter {
+    
+    def apply[T](implicit ev: JsonWriter[T]): JsonWriter[T] = ev
+    
+    def from[T](f: T => JsValue): JsonWriter[T] = (v: T) => f(v)
+
+    implicit val jsonStringWriter: JsonWriter[String] = from(v => JsString(v))
+    implicit val jsonIntWriter: JsonWriter[Int] =  from(v => JsNumber(v))
+    implicit def jsonOptWriter[T](implicit jw : JsonWriter[T]): JsonWriter[Option[T]] = {
+      case Some(value) => toJson(value)
+      case None => JsNull
+    }
+  }
+
+  def toJson[T: JsonWriter](v: T): JsValue = implicitly(JsonWriter[T]).toJson(v)
+  
+  toJson("vffv")
+  toJson(10)
+  toJson(Option(10))
+  toJson(Some(10))
+
 
   //  "fvhfujhubvf".toJson
-  //  10.toJson
-   println( toJson( Option(10)))
-  //  Option("vdfvf").toJson
-
+//  10.toJson
+//  Option(10).toJson
+//  Option("vdfvf").toJson
+  
   
 
 
@@ -51,11 +65,9 @@ object type_classes extends App {
       override def less(a: A, b: A): Boolean = f(a, b)
     }
 
-    given Ordering[Int] = from[Int](_ < _)
-
-    given Ordering[String] = from[String](_.length < _.length)
-
-    given Ordering[User] = from[User](_.age < _.age)
+     implicit val orderingInt : Ordering[Int] = from(_ < _)
+     implicit val stringInt: Ordering[String] = from(_.length < _.length)
+     implicit val userInt: Ordering[User] = from(_.age < _.age)
   }
 
   case class User(name: String, age: Int)
